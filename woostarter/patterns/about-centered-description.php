94e5d2ae43362b265a4c6f4c1f558c2f30eeb51a
Beatriz Fialho <beatriz.fialho@automattic.com>
2025-02-11 14:33:40 +0000

    Add "Centered description" pattern
    As mentioned in #5

---
+++ woostarter/patterns/about-centered-description.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Centered description
+ * Slug: woostarter/text-centered-description
+ * Categories: text, about
+ * Keywords: about, text
+ * Description: A centered description section.
+ *
+
+?>
+<!-- wp:group {"metadata":{"name":"Centered description"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","align":"wide","fontSize":"x-large"} -->
+<h2 class="wp-block-heading alignwide has-text-align-center has-x-large-font-size">Kiri is a destination for art lovers, design enthusiasts, and collectors. We curate an eclectic mix of collectible vintage treasures and contemporary creations by talented artists from across the globe.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
