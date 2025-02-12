58c2ed9fdd5fcbbc94ebee0c7849e7d26e7aa4fb
Beatriz Fialho <beatriz.fialho@automattic.com>
2025-02-12 17:20:25 +0000

    Add "Background image, offset text and link" pattern
    Add "Background image, offset text and link" pattern from #5

---
+++ woostarter/patterns/about-background-image-offset-text.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Background image, offset text and link
+ * Slug: woostarter/background-image-offset-text-link
+ * Categories: text, about, media
+ * Keywords: about, text, icons, cover, story
+ * Description: A section with a background image and some offset text.
+ *
+
+?>
+<!-- wp:cover {"url":"https://kiri.mystagingwebsite.com/wp-content/uploads/2024/11/home-pattern-cover-image-scaled-1.jpg","id":217,"alt":"A black and white abstract composition of curved paper with light and shadow.","dimRatio":30,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","sizeSlug":"full","metadata":{"name":"Background image, offset text and link"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;min-height:100vh"><img class="wp-block-cover__image-background wp-image-217 size-full" alt="A black and white abstract composition of curved paper with light and shadow." src="https://kiri.mystagingwebsite.com/wp-content/uploads/2024/11/home-pattern-cover-image-scaled-1.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-30 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","fontSize":"large"} -->
+<h2 class="wp-block-heading has-text-align-left has-large-font-size">Our ethos: the <em>undefined</em></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size"><a href="#">Learn more</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
