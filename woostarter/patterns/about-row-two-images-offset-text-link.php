0461d8d52433cef774dee380d78d31f67511809d
Beatriz Fialho <beatriz.fialho@automattic.com>
2025-02-13 11:15:29 +0000

    Add "Two image row, offset text and link" pattern
    Add "Two image row, offset text and link" pattern from #5

---
+++ woostarter/patterns/about-row-two-images-offset-text-link.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Title: Two image row, offset text and link
+ * Slug: woostarter/two-image-row-offset-text-link
+ * Categories: text, about, media
+ * Keywords: about, text
+ * Description: A section with two images, offset text and link.
+ *
+
+?>
+<!-- wp:group {"metadata":{"categories":["intro"],"name":"Two image row, offset text and link","patternName":"a8c/intro-two-column-with-image-and-content"},"align":"full","className":"alignfull is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"isStackedOnMobile":false,"align":"wide"} -->
+<div class="wp-block-columns alignwide is-not-stacked-on-mobile"><!-- wp:column {"width":"70%"} -->
+<div class="wp-block-column" style="flex-basis:70%"><!-- wp:cover {"url":"https://kiri.mystagingwebsite.com/wp-content/uploads/2025/02/pattern-vintage-bracelet.jpg","id":483,"alt":"A vintage silver cuff bracelet with square turquoise stone inlays.","dimRatio":0,"isDark":false,"style":{"color":[]}} -->
+<div class="wp-block-cover is-light"><img class="wp-block-cover__image-background wp-image-483" alt="A vintage silver cuff bracelet with square turquoise stone inlays." src="https://kiri.mystagingwebsite.com/wp-content/uploads/2025/02/pattern-vintage-bracelet.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"30%"} -->
+<div class="wp-block-column" style="flex-basis:30%"><!-- wp:cover {"url":"https://kiri.mystagingwebsite.com/wp-content/uploads/2025/02/pattern-fabric-close-up.jpg","id":453,"alt":"A close-up of loosely woven burlap fabric with a rough texture.","dimRatio":0,"isDark":false,"style":{"color":[]}} -->
+<div class="wp-block-cover is-light"><img class="wp-block-cover__image-background wp-image-453" alt="A close-up of loosely woven burlap fabric with a rough texture." src="https://kiri.mystagingwebsite.com/wp-content/uploads/2025/02/pattern-fabric-close-up.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","fontSize":"large"} -->
+<h2 class="wp-block-heading has-text-align-left has-large-font-size">Our ethos: the <em>undefined</em></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size"><a href="#">Learn more</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
