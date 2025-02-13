3edcd01ed063a2a1ab5fea9f97b0b9f7b4ffbd9b
Beatriz Fialho <beatriz.fialho@automattic.com>
2025-02-13 11:32:14 +0000

    Add "Three-column gallery, variable height" pattern
    Add "Three-column gallery, variable height" pattern from #5

---
+++ woostarter/patterns/about-3-image-gallery.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Three-column gallery, variable height
+ * Slug: woostarter/3-image-gallery
+ * Categories: about, media
+ * Keywords: about, gallery
+ * Description: A three-column gallery section.
+ *
+
+?>
+<!-- wp:group {"metadata":{"name":"3-column gallery, variable height"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":436,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://kiri.mystagingwebsite.com/wp-content/uploads/2025/02/pattern-vintage-rug.jpg" alt="A dark fabric with an intricate golden pattern depicting figures, animals, and trees." class="wp-image-436"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":423,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://kiri.mystagingwebsite.com/wp-content/uploads/2025/02/pattern-ceramic-cup.jpg" alt="A glossy, hand-crafted brown ceramic cup with an organic shape." class="wp-image-423"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":437,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://kiri.mystagingwebsite.com/wp-content/uploads/2025/02/pattern-vintage-necklace.jpg" alt="A handmade necklace with large, irregularly shaped stone beads in earthy tones." class="wp-image-437"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
