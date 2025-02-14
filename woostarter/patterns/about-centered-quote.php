886666c6b740ec2d642add0ec79e18395e267293
Beatriz Fialho <beatriz.fialho@automattic.com>
2025-02-14 12:34:05 +0000

    Add "Centered quote" pattern
    Add "Centered quote" pattern from #5

---
+++ woostarter/patterns/about-centered-quote.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Centered quote
+ * Slug: woostarter/text-centered-quote
+ * Categories: text, about, quotes
+ * Keywords: about, text, quote
+ * Description: A centered quote.
+ *
+
+?>
+<!-- wp:group {"metadata":{"name":"Centered Quote"},"align":"full","className":"is-style-section-3","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-section-3" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","align":"wide","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading alignwide has-text-align-center has-xx-large-font-size">“At Kiri, we celebrate the charm of the imperfect, inviting you to see the artistry in forms that are raw, organic, and unique.”</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Luiza Saito, <em>Founder</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
