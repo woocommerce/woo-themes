09bc5b21b72d574ff12a0e16942a901bd59dbff7
MaggieCabrera <maggie.cabrera@automattic.com>
2025-04-29 10:17:25 +0200

    add pattern and assets

---
+++ woostarter/patterns/reviews-video-and-testimonials.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Title: Reviews video and testimonials
+ * Slug: woostarter/reviews-video-and-testimonials
+ * Categories: 
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Reviews: Video and testimonials row"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0"><!-- wp:spacer {"height":"var:preset|spacing|70"} -->
+<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center"><?php esc_html_e('Product Reviews', 'woostarter');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('What our customers are saying.', 'woostarter');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns {"verticalAlignment":"bottom","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom","className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-bottom is-style-default" style="padding-top:0;padding-bottom:0"><!-- wp:video {"id":570,"style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<figure style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0" class="wp-block-video"><video controls src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/videos/patterns-video-review.mp4"></video></figure>
+<!-- /wp:video --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","className":"is-style-section-1"} -->
+<div class="wp-block-column is-vertically-aligned-bottom is-style-section-1"><!-- wp:cover {"overlayColor":"theme-2","isUserOverlayColor":true,"contentPosition":"top left","isDark":false,"style":{"dimensions":{"aspectRatio":"9/16"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-top-left" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-theme-2-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"is-style-section-1","style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group is-style-section-1" style="min-height:100%;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"className":"is-style-default","style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-default"><!-- wp:jetpack/rating-star {"rating":2} -->
+<figure class="wp-block-jetpack-rating-star" style="text-align:left"><span>★</span><span>★</span></figure>
+<!-- /wp:jetpack/rating-star -->
+
+<!-- wp:paragraph {"align":"left","placeholder":"Write title…","fontSize":"medium"} -->
+<p class="has-text-align-left has-medium-font-size"><?php esc_html_e('“Great product, color as advertised.”', 'woostarter');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"width":"0px","style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div style="width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php esc_html_e('Ella, Manchester', 'woostarter');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hero-background-image.webp","alt":"Three minimalist ceramic bowls with pedestal bases in soft white and light blue tones.","dimRatio":40,"overlayColor":"theme-5","isUserOverlayColor":true,"minHeight":49.3,"minHeightUnit":"vh","contentPosition":"top left","sizeSlug":"full","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"dimensions":{"aspectRatio":"9/16"}}} -->
+<div class="wp-block-cover has-custom-content-position is-position-top-left" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:49.3vh"><img class="wp-block-cover__image-background  size-full" alt="<?php esc_attr_e('Three minimalist ceramic bowls with pedestal bases in soft white and light blue tones.', 'woostarter');?>" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hero-background-image.webp" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-theme-5-background-color has-background-dim-40 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="min-height:100%"><!-- wp:group {"className":"is-style-default","style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group is-style-default" style="min-height:100%;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"className":"is-style-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-default"><!-- wp:jetpack/rating-star {"rating":5} -->
+<figure class="wp-block-jetpack-rating-star" style="text-align:left"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></figure>
+<!-- /wp:jetpack/rating-star -->
+
+<!-- wp:paragraph {"align":"left","placeholder":"Write title…","fontSize":"medium"} -->
+<p class="has-text-align-left has-medium-font-size"><?php esc_html_e('“I cannot point out one flaw thoughout the whole purchase process.”', 'woostarter');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"width":"0px","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<div style="width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php esc_html_e('Jonathan, Norwich', 'woostarter');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","className":"is-style-section-1"} -->
+<div class="wp-block-column is-vertically-aligned-bottom is-style-section-1"><!-- wp:cover {"overlayColor":"theme-2","isUserOverlayColor":true,"contentPosition":"top left","isDark":false,"style":{"dimensions":{"aspectRatio":"9/16"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-top-left" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-theme-2-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"is-style-section-1","style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group is-style-section-1" style="min-height:100%;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"className":"is-style-default","style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-default"><!-- wp:jetpack/rating-star {"rating":5} -->
+<figure class="wp-block-jetpack-rating-star" style="text-align:left"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></figure>
+<!-- /wp:jetpack/rating-star -->
+
+<!-- wp:paragraph {"align":"left","placeholder":"Write title…","fontSize":"medium"} -->
+<p class="has-text-align-left has-medium-font-size"><?php esc_html_e('“Delivery went smoothly as expected.”', 'woostarter');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"width":"0px","style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div style="width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php esc_html_e('Ella, Manchester', 'woostarter');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|70"} -->
+<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
