e23a0d6f1594bbaa6d320b534964097606c9d7f8
Maggie Cabrera <maggie@onemaggie.com>
2025-05-30 11:20:07 +0200

    more template tweaks and pattern creation

---
+++ woostarter/patterns/order-confirmation.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Title: WooCommerce Order Confirmation
+ * Slug: woostarter/order-confirmation
+ * Categories: page
+ * Keywords: page
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ */
+?>
+<!-- wp:group {"tagName":"main","layout":{"inherit":true,"type":"constrained"}} -->
+<main class="wp-block-group">
+	
+<!-- wp:spacer {"height":"var:preset|spacing|50"} -->
+<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:woocommerce/order-confirmation-status {"fontSize":"medium"} /-->
+
+<!-- wp:woocommerce/order-confirmation-summary {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} /-->
+
+<!-- wp:woocommerce/order-confirmation-totals-wrapper {"align":"wide"} -->
+<!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size"><?php esc_html_e('Order details', 'woostarter');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:woocommerce/order-confirmation-totals {"lock":{"remove":true},"borderColor":"theme-6","style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|50"}}}} /-->
+<!-- /wp:woocommerce/order-confirmation-totals-wrapper -->
+
+<!-- wp:woocommerce/order-confirmation-downloads-wrapper {"align":"wide"} -->
+<!-- wp:heading {"style":{"typography":{"fontSize":"24px"}}} -->
+<h2 class="wp-block-heading" style="font-size:24px"><?php esc_html_e('Downloads', 'woostarter');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:woocommerce/order-confirmation-downloads {"lock":{"remove":true},"borderColor":"theme-6","style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|50"}}}} /-->
+<!-- /wp:woocommerce/order-confirmation-downloads-wrapper -->
+
+<!-- wp:columns {"align":"wide","className":"wc-block-order-confirmation-address-wrapper","style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|50"},"blockGap":{"left":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns alignwide wc-block-order-confirmation-address-wrapper" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:woocommerce/order-confirmation-shipping-wrapper {"align":"wide"} -->
+<!-- wp:heading {"style":{"typography":{"fontSize":"24px"}}} -->
+<h2 class="wp-block-heading" style="font-size:24px"><?php esc_html_e('Shipping address', 'woostarter');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:woocommerce/order-confirmation-shipping-address {"lock":{"remove":true},"borderColor":"theme-6","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} /-->
+<!-- /wp:woocommerce/order-confirmation-shipping-wrapper --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:woocommerce/order-confirmation-billing-wrapper {"align":"wide"} -->
+<!-- wp:heading {"style":{"typography":{"fontSize":"24px"}}} -->
+<h2 class="wp-block-heading" style="font-size:24px"><?php esc_html_e('Billing address', 'woostarter');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:woocommerce/order-confirmation-billing-address {"lock":{"remove":true},"borderColor":"theme-6","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} /-->
+<!-- /wp:woocommerce/order-confirmation-billing-wrapper --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:woocommerce/order-confirmation-additional-fields-wrapper {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}}} -->
+<!-- wp:heading {"style":{"typography":{"fontSize":"24px"}}} -->
+<h2 class="wp-block-heading" style="font-size:24px"><?php esc_html_e('Additional information', 'woostarter');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:woocommerce/order-confirmation-additional-fields /-->
+<!-- /wp:woocommerce/order-confirmation-additional-fields-wrapper -->
+
+<!-- wp:woocommerce/order-confirmation-additional-information /-->
+
+<!-- wp:spacer {"height":"var:preset|spacing|60"} -->
+<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></main>
+<!-- /wp:group -->
