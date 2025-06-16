ac4f1762e6278c2d33f180489f90920d10f66fca
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-16 17:41:19 +0200

    fix attr function on alt tags

--- woostarter/patterns/about-left-aligned-image-right-text.php
+++ woostarter/patterns/about-left-aligned-image-right-text.php
@@ -18,8 +18,8 @@
 <div class="wp-block-columns alignwide are-vertically-aligned-center has-theme-5-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%">
 	
-<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/beanie-a.webp","alt":"<?php esc_html_e( 'An illustration of a wool beanie', 'woostarter' ); ?>","dimRatio":10,"isDark":false,"style":{"dimensions":{"aspectRatio":"4/3"}},"layout":{"type":"default"}} -->
-<div class="wp-block-cover is-light"><img class="wp-block-cover__image-background" alt="<?php esc_html_e( 'An illustration of a wool beanie', 'woostarter' ); ?>" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/beanie-a.webp" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-10 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/beanie-a.webp","alt":"<?php esc_attr_e( 'An illustration of a wool beanie', 'woostarter' ); ?>","dimRatio":10,"isDark":false,"style":{"dimensions":{"aspectRatio":"4/3"}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light"><img class="wp-block-cover__image-background" alt="<?php esc_attr_e( 'An illustration of a wool beanie', 'woostarter' ); ?>" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/beanie-a.webp" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-10 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
 	<p class="has-text-align-center has-large-font-size"></p>
 	<!-- /wp:paragraph --></div></div>
 	<!-- /wp:cover -->
