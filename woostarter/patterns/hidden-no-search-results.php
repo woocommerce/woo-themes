5247af178fda0524b74466f09c869f8d4d61e171
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-20 17:52:38 +0200

    fix color and alignment of no product text

--- woostarter/patterns/hidden-no-search-results.php
+++ woostarter/patterns/hidden-no-search-results.php
@@ -5,7 +5,6 @@
  * Inserter: no
  */
 ?>
-
-<!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><?php echo esc_html_x( 'No results were found using that search', 'Text explaining that there are no results returned from a search', 'woostarter' ); ?>.</p>
+<!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-4"}}}},"textColor":"theme-4"} -->
+<p class="has-text-align-left has-theme-4-color has-text-color has-link-color"><?php echo esc_html_x( 'No results were found using that search', 'Text explaining that there are no results returned from a search', 'woostarter' ); ?>.</p>
 <!-- /wp:paragraph -->
