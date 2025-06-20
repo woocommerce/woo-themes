5f939308494704af27db96ae3b9cee89ca5f383f
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-20 10:39:13 +0200

    removed instagram link

--- woostarter/patterns/social-media-call-to-action-with-hero-image.php
+++ woostarter/patterns/social-media-call-to-action-with-hero-image.php
@@ -13,7 +13,7 @@
 
 <!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"300"},"elements":{"link":{"color":{"text":"var:preset|color|theme-1"}}}},"textColor":"theme-1","fontSize":"medium"} -->
 <p class="has-text-align-center has-theme-1-color has-text-color has-link-color has-medium-font-size" style="font-style:normal;font-weight:300"><?php /* Translators: 1. is the start of a 'a' HTML element, 2. is the end of a 'a' HTML element */ 
-echo sprintf( esc_html__( '%1$s@example%2$s', 'woostarter' ), '<a href="' . esc_url( 'https://www.instagram.com/' ) . '">', '</a>' ); ?></p>
+echo sprintf( esc_html__( '%1$s@example%2$s', 'woostarter' ), '<a href="#">', '</a>' ); ?></p>
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:cover --></div>
 <!-- /wp:group -->
