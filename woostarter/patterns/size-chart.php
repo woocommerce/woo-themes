e93b9f9cffe826dfa404a6c4fe90b2e481ca8b4e
Sarah Norris <sarah@sekai.co.uk>
2025-06-23 14:44:13 +0100

    Add Size Chart pattern

---
+++ woostarter/patterns/size-chart.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Title: Size chart
+ * Slug: woostarter/size-chart
+ * Categories: woocommerce
+ * Keywords: size chart, table
+ * Description: A size chart table.
+ * Block Types: core/table
+ */
+?>
+
+<!-- wp:spacer {"height":"var:preset|spacing|60","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"textColor":"theme-4"} -->
+<p class="has-theme-4-color has-text-color"><?php esc_html_e('These size charts are a guide to help you find the right size for you. As many pieces are designed in a unique way, we suggest you also refer to the specific garment lengths and measurements provided in the product descriptions.', 'woostarter');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:table {"className":"is-style-stripes"} -->
+<figure class="wp-block-table is-style-stripes"><table class="has-fixed-layout"><tbody><tr><td class="has-text-align-center" data-align="center"><?php esc_html_e('Size', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('EU', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('UK', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('FR', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('IT', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('Chest', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('Waist', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('Arm Length', 'woostarter');?></td><td class="has-text-align-center" data-align="center"><?php esc_html_e('Low Hip', 'woostarter');?></td></tr><tr><td class="has-text-align-center" data-align="center"><?php esc_html_e('XS', 'woostarter');?></td><td class="has-text-align-center" data-align="center">32</td><td class="has-text-align-center" data-align="center">4</td><td class="has-text-align-center" data-align="center">34</td><td class="has-text-align-center" data-align="center">38</td><td class="has-text-align-center" data-align="center">76</td><td class="has-text-align-center" data-align="center">60</td><td class="has-text-align-center" data-align="center">59.7</td><td class="has-text-align-center" data-align="center">75</td></tr><tr><td class="has-text-align-center" data-align="center"><?php esc_html_e('S', 'woostarter');?></td><td class="has-text-align-center" data-align="center">34</td><td class="has-text-align-center" data-align="center">6</td><td class="has-text-align-center" data-align="center">36</td><td class="has-text-align-center" data-align="center">40</td><td class="has-text-align-center" data-align="center">80</td><td class="has-text-align-center" data-align="center">64</td><td class="has-text-align-center" data-align="center">59.6</td><td class="has-text-align-center" data-align="center">79</td></tr><tr><td class="has-text-align-center" data-align="center"><?php esc_html_e('M', 'woostarter');?></td><td class="has-text-align-center" data-align="center">36</td><td class="has-text-align-center" data-align="center">8</td><td class="has-text-align-center" data-align="center">38</td><td class="has-text-align-center" data-align="center">42</td><td class="has-text-align-center" data-align="center">84</td><td class="has-text-align-center" data-align="center">68</td><td class="has-text-align-center" data-align="center">59.8</td><td class="has-text-align-center" data-align="center">83</td></tr><tr><td class="has-text-align-center" data-align="center"><?php esc_html_e('L', 'woostarter');?></td><td class="has-text-align-center" data-align="center">38</td><td class="has-text-align-center" data-align="center">10</td><td class="has-text-align-center" data-align="center">40</td><td class="has-text-align-center" data-align="center">44</td><td class="has-text-align-center" data-align="center">88</td><td class="has-text-align-center" data-align="center">72</td><td class="has-text-align-center" data-align="center">60.0</td><td class="has-text-align-center" data-align="center">87</td></tr><tr><td class="has-text-align-center" data-align="center"><?php esc_html_e('XL', 'woostarter');?></td><td class="has-text-align-center" data-align="center">42</td><td class="has-text-align-center" data-align="center">12</td><td class="has-text-align-center" data-align="center">42</td><td class="has-text-align-center" data-align="center">46</td><td class="has-text-align-center" data-align="center">92</td><td class="has-text-align-center" data-align="center">76</td><td class="has-text-align-center" data-align="center">60.2</td><td class="has-text-align-center" data-align="center">91</td></tr></tbody></table></figure>
+<!-- /wp:table -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php esc_html_e('How to measure', 'woostarter');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"theme-4"} -->
+<p class="has-theme-4-color has-text-color"><?php esc_html_e('CHEST: Measure around the fullest part of your chest, keeping the tape measure horizontal.', 'woostarter');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"theme-4"} -->
+<p class="has-theme-4-color has-text-color"><?php esc_html_e('WAIST :Measure around your natural waistline, at the narrowest point.', 'woostarter');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"theme-4"} -->
+<p class="has-theme-4-color has-text-color"><?php esc_html_e('ARM LENGTH: Measure from shoulder point to just below the wrist.', 'woostarter');?>   </p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|60","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
