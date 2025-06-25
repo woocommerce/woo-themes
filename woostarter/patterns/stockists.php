a64d17d4e40f3941bd79e9f2757ae30441286a1f
Sarah Norris <sarah@sekai.co.uk>
2025-06-25 16:07:02 +0100

    Add extra group wrapper and heading

--- woostarter/patterns/stockists.php
+++ woostarter/patterns/stockists.php
@@ -9,8 +9,13 @@
  */
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:paragraph -->
+<!-- wp:group {"metadata":{"name":"Stockists"},"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"metadata":{"categories":["woocommerce"],"patternName":"woostarter/stockists"},"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide">
+<!-- wp:heading {"level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-2"}}}},"textColor":"theme-2"} -->
+<h1 class="wp-block-heading has-theme-2-color has-text-color has-link-color"><?php esc_html_e( 'Stockists', 'woostarter' ); ?></h1>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
 <p><?php esc_html_e( 'Where we\'re at.', 'woostarter' ); ?></p>
 <!-- /wp:paragraph -->
 
@@ -21,8 +26,8 @@
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"metadata":{"name":"Europe"},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e( 'Europe', 'woostarter' ); ?></h2>
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-2"}}}},"textColor":"theme-2"} -->
+<h2 class="wp-block-heading has-theme-2-color has-text-color has-link-color"><?php esc_html_e( 'Europe', 'woostarter' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns -->
@@ -85,8 +90,8 @@
 <!-- /wp:spacer -->
 
 <!-- wp:group {"metadata":{"name":"Asia \u0026 Australia"},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e( 'Asia &amp; Australia', 'woostarter' ); ?></h2>
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-2"}}}},"textColor":"theme-2"} -->
+<h2 class="wp-block-heading has-theme-2-color has-text-color has-link-color"><?php esc_html_e( 'Asia &amp; Australia', 'woostarter' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns -->
@@ -147,8 +152,8 @@
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"metadata":{"name":"North America"},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e( 'North America', 'woostarter' ); ?></h2>
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-2"}}}},"textColor":"theme-2"} -->
+<h2 class="wp-block-heading has-theme-2-color has-text-color has-link-color"><?php esc_html_e( 'North America', 'woostarter' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns -->
@@ -211,8 +216,8 @@
 <!-- /wp:spacer -->
 
 <!-- wp:group {"metadata":{"name":"South America"},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e( 'South America', 'woostarter' ); ?></h2>
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-2"}}}},"textColor":"theme-2"} -->
+<h2 class="wp-block-heading has-theme-2-color has-text-color has-link-color"><?php esc_html_e( 'South America', 'woostarter' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns -->
@@ -271,4 +276,5 @@
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
