8a97fc3fead0c1d5b2f27a7382ae5ecd733695d8
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-25 13:05:54 +0200

    made design changes to the footer

--- woostarter/patterns/footer.php
+++ woostarter/patterns/footer.php
@@ -6,8 +6,7 @@
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Default Footer"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-default" style="padding-top:0;padding-bottom:0"><!-- wp:spacer {"height":"var:preset|spacing|50"} -->
+<!-- wp:group {"tagName":"footer","metadata":{"name":"Default Footer"},"align":"full","className":"is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} --><footer class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:spacer {"height":"var:preset|spacing|50"} -->
 <div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
@@ -15,11 +14,19 @@
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"40%"} -->
 <div class="wp-block-column" style="flex-basis:40%"><!-- wp:site-title {"level":0} /-->
 
-<!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-3"}}}},"textColor":"theme-3","fontSize":"small"} -->
-<p class="has-text-align-left has-theme-3-color has-text-color has-link-color has-small-font-size"><?php echo wp_kses_post(
-    __('Bold knitwear designed with care<br>—for you and the planet.', 'woostarter')
-);?></p>
-<!-- /wp:paragraph --></div>
+<!-- wp:site-tagline {"style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-4"}}}},"textColor":"theme-4"} /-->
+
+<!-- wp:spacer {"height":"var:preset|spacing|30"} -->
+<div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:social-links {"iconColor":"theme-2","iconColorValue":"#111111","size":"has-normal-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|30"}}}} -->
+<ul class="wp-block-social-links has-normal-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"x"} /-->
+
+<!-- wp:social-link {"url":"#","service":"whatsapp"} /--></ul>
+<!-- /wp:social-links --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"30%"} -->
@@ -31,9 +38,12 @@
 <h3 class="wp-block-heading has-text-align-left has-medium-font-size"><?php esc_html_e('Shop', 'woostarter');?></h3>
 <!-- /wp:heading -->
 
+<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group">
-<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <!-- wp:navigation-link {"label":"<?php esc_html_e('Ceramics', 'woostarter');?>","url":"#"} /-->
 
 <!-- wp:navigation-link {"label":"<?php esc_html_e('Furniture', 'woostarter');?>","url":"#"} /-->
@@ -41,19 +51,21 @@
 <!-- wp:navigation-link {"label":"<?php esc_html_e('Accessories', 'woostarter');?>","url":"#"} /-->
 
 <!-- wp:navigation-link {"label":"<?php esc_html_e('Clothing', 'woostarter');?>","url":"#"} /-->
-<!-- /wp:navigation -->
-</div>
+<!-- /wp:navigation --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"15%","layout":{"type":"default"}} -->
 <div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"textAlign":"left","level":3,"fontSize":"medium"} -->
-<h3 class="wp-block-heading has-text-align-left has-medium-font-size">Info</h3>
+<h3 class="wp-block-heading has-text-align-left has-medium-font-size"><?php esc_html_e('Info', 'woostarter');?></h3>
 <!-- /wp:heading -->
 
+<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group">
-<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <!-- wp:navigation-link {"label":"<?php esc_html_e('About', 'woostarter');?>","url":"#"} /-->
 
 <!-- wp:navigation-link {"label":"<?php esc_html_e('Contact us', 'woostarter');?>","url":"#"} /-->
@@ -61,8 +73,7 @@
 <!-- wp:navigation-link {"label":"<?php esc_html_e('FAQs', 'woostarter');?>","url":"#"} /-->
 
 <!-- wp:navigation-link {"label":"<?php esc_html_e('Blog', 'woostarter');?>","url":"#"} /-->
-<!-- /wp:navigation -->
-</div>
+<!-- /wp:navigation --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
@@ -71,41 +82,27 @@
 <div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:paragraph -->
-<p>[Shopping icons]</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('United Kingdom (GBP £)', 'woostarter');?></a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group -->
-
 <!-- wp:spacer {"height":"var:preset|spacing|20"} -->
 <div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull">
-
-<!-- wp:spacer {"height":"var:preset|spacing|40","style":{"layout":[]}} -->
-<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:spacer {"height":"var:preset|spacing|60","style":{"layout":[]}} -->
+<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|theme-3"}}}},"textColor":"theme-3","fontSize":"small"} -->
-<p class="has-text-align-left has-theme-3-color has-text-color has-link-color has-small-font-size" style="text-transform:uppercase"><?php esc_html_e('© 2025 Elke', 'woostarter');?></p>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull" style="padding-right:0;padding-left:0"><!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|theme-4"}}}},"textColor":"theme-4","fontSize":"small"} -->
+<p class="has-text-align-left has-theme-4-color has-text-color has-link-color has-small-font-size" style="text-transform:uppercase">© 2025 <?php esc_html_e('Elke', 'woostarter');?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-3"}}}},"textColor":"theme-3","fontSize":"small"} -->
-<p class="has-text-align-left has-theme-3-color has-text-color has-link-color has-small-font-size"><?php esc_html_e('Designed with WordPress', 'woostarter');?></p>
+<!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-4"}}}},"textColor":"theme-4","fontSize":"small"} -->
+<p class="has-text-align-left has-theme-4-color has-text-color has-link-color has-small-font-size"><?php esc_html_e('Designed with WordPress', 'woostarter');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:spacer {"height":"var:preset|spacing|40","style":{"layout":[]}} -->
-<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- wp:spacer {"height":"var:preset|spacing|60","style":{"layout":[]}} -->
+<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer --></div>
-<!-- /wp:group --></div>
+<!-- /wp:group --></footer>
 <!-- /wp:group -->
