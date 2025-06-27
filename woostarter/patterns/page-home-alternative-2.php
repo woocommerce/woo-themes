4008e23fd6ca3311a8e9223e1b6b6d7f1120919c
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-27 11:51:22 +0200

    add initial pattern

---
+++ woostarter/patterns/page-home-alternative-2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Shop homepage alternative 2
+ * Slug: woostarter/page-home-alternative-2
+ * Categories: woocommerce
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A shop homepage pattern.
+ *
+ */
+?>
+<!-- wp:pattern {"slug":"woostarter/two-columns-full-width-categories"} /-->
+
+<!-- wp:pattern {"slug":"woostarter/new-arrival-product"} /-->
+
+<!-- wp:pattern {"slug":"woostarter/woocommerce-featured-products"} /-->
+
+<!-- wp:pattern {"slug":"woostarter/left-aligned-image-description-button"} /-->
+
+<!-- wp:pattern {"slug":"woostarter/store-features-row-icons-4-columns"} /-->
