56d68b8a4188966745cc9a30cc93c3e195b93707
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-27 16:30:25 +0200

    translateable strings

--- woostarter/patterns/subscribe-to-newsletter.php
+++ woostarter/patterns/subscribe-to-newsletter.php
@@ -23,9 +23,9 @@
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:jetpack/contact-form {"className":"is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}}}} -->
-<div class="wp-block-jetpack-contact-form is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:jetpack/field-email {"label":"","required":true,"requiredText":"","placeholder":"Your email","width":75,"borderRadius":0,"borderWidth":1,"labelFontSize":"14px"} /-->
+<div class="wp-block-jetpack-contact-form is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:jetpack/field-email {"label":"","required":true,"requiredText":"","placeholder":"<?php esc_html_e('Your email', 'woostarter');?>","width":75,"borderRadius":0,"borderWidth":1,"labelFontSize":"14px"} /-->
 
-<!-- wp:jetpack/button {"element":"button","text":"Subscribe","borderRadius":0,"width":"150px","lock":{"move":true,"remove":true}} /--></div>
+<!-- wp:jetpack/button {"element":"button","text":"<?php esc_html_e('Subscribe', 'woostarter');?>","borderRadius":0,"width":"150px","lock":{"move":true,"remove":true}} /--></div>
 <!-- /wp:jetpack/contact-form --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
