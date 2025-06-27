31c724a3e1e4daa21d6a65b9b703a18fad27f6ff
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-27 17:31:24 +0200

    created pattern

---
+++ woostarter/patterns/page-about-with-store-information.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: About page with store information
+ * Slug: woostarter/page-about-with-store-information
+ * Categories: woocommerce
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: An about page pattern.
+ *
+ */
+?>
+
+<!-- wp:pattern {"slug":"woostarter/store-features-three-columns"} /-->
