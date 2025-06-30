fc07c96503cc6fb938923731b6d8fc94e4227294
MaggieCabrera <maggie.cabrera@automattic.com>
2025-06-30 12:23:02 +0200

    adapt pattern to new design

--- woostarter/patterns/about-centered-text.php
+++ woostarter/patterns/about-centered-text.php
@@ -7,14 +7,14 @@
  * Description: A description section aligned center.
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Centered text"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<!-- wp:group {"metadata":{"name":"Centered text"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|70","right":"var:preset|spacing|70"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--70);padding-bottom:0;padding-left:var(--wp--preset--spacing--70)"><!-- wp:spacer {"height":"var:preset|spacing|80"} -->
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:heading {"textAlign":"left","align":"wide","fontSize":"x-large"} -->
-<h2 class="wp-block-heading alignwide has-text-align-left has-x-large-font-size"><?php esc_html_e('Our products are crafted with meticulous attention to quality, using only the finest materials and ingredients. Designed to deliver exceptional performance, each item undergoes rigorous testing to ensure it meets the highest standards.', 'woostarter');?></h2>
-<!-- /wp:heading -->
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|theme-2"}}}},"textColor":"theme-2","fontSize":"large"} -->
+<p class="has-text-align-center has-theme-2-color has-text-color has-link-color has-large-font-size"><?php esc_html_e('Our products are crafted with meticulous attention to quality, using only the finest materials and ingredients. Designed to deliver exceptional performance, each item undergoes rigorous testing to ensure it meets the highest standards.', 'woostarter');?></p>
+<!-- /wp:paragraph -->
 
 <!-- wp:spacer {"height":"var:preset|spacing|80"} -->
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
